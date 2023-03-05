e6473947f884bca397496117f0cfaebd37b15333
Timmy <93844165+Gotterbote@users.noreply.github.com>
2023-03-05 09:11:29 -0600

    Add project files.

---
+++ Loop Tests/frmMain.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnUserInput = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btnDoUntil = New System.Windows.Forms.Button()
+        Me.btnDoWhile = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.lstContainer = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnUserInput
+        '
+        Me.btnUserInput.Location = New System.Drawing.Point(54, 259)
+        Me.btnUserInput.Name = "btnUserInput"
+        Me.btnUserInput.Size = New System.Drawing.Size(65, 23)
+        Me.btnUserInput.TabIndex = 17
+        Me.btnUserInput.Text = "User Input"
+        Me.btnUserInput.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(54, 74)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(40, 23)
+        Me.btn2.TabIndex = 16
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btnDoUntil
+        '
+        Me.btnDoUntil.Location = New System.Drawing.Point(54, 230)
+        Me.btnDoUntil.Name = "btnDoUntil"
+        Me.btnDoUntil.Size = New System.Drawing.Size(65, 23)
+        Me.btnDoUntil.TabIndex = 15
+        Me.btnDoUntil.Text = "Do Until"
+        Me.btnDoUntil.UseVisualStyleBackColor = True
+        '
+        'btnDoWhile
+        '
+        Me.btnDoWhile.Location = New System.Drawing.Point(54, 201)
+        Me.btnDoWhile.Name = "btnDoWhile"
+        Me.btnDoWhile.Size = New System.Drawing.Size(65, 23)
+        Me.btnDoWhile.TabIndex = 14
+        Me.btnDoWhile.Text = "Do While"
+        Me.btnDoWhile.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(54, 172)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(65, 23)
+        Me.btn5.TabIndex = 13
+        Me.btn5.Text = "5 - Clear"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(54, 130)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(40, 23)
+        Me.btn4.TabIndex = 12
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(54, 101)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(40, 23)
+        Me.btn3.TabIndex = 11
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(54, 49)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(40, 23)
+        Me.btn1.TabIndex = 10
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'lstContainer
+        '
+        Me.lstContainer.FormattingEnabled = True
+        Me.lstContainer.Location = New System.Drawing.Point(222, 44)
+        Me.lstContainer.Name = "lstContainer"
+        Me.lstContainer.Size = New System.Drawing.Size(126, 238)
+        Me.lstContainer.TabIndex = 18
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(477, 355)
+        Me.Controls.Add(Me.lstContainer)
+        Me.Controls.Add(Me.btnUserInput)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btnDoUntil)
+        Me.Controls.Add(Me.btnDoWhile)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "frmMain"
+        Me.Text = "Loop Tests"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnUserInput As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btnDoUntil As Button
+    Friend WithEvents btnDoWhile As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents lstContainer As ListBox
+End Class
---
+++ Loop Tests/frmMain.vb
@@ -0,0 +1,123 @@
+﻿Public Class frmMain
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Dim strPayRate As String = ""
+        strPayRate = InputBox("Enter in the hourly rate of pay", "Enter Pay Rate", "12.50")
+        MessageBox.Show(strPayRate)
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        Dim intX As Integer
+
+        For intX = 1 To 5
+            MessageBox.Show(intX)
+        Next
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        Dim intX As Integer
+        Dim intTotal As Integer
+        Dim strMessage As String = ""
+
+        For intX = 1 To 10
+            strMessage = strMessage + intX.ToString("n0") + " "
+            intTotal = intTotal + intX
+        Next
+
+        MessageBox.Show("My Output: " + strMessage + vbCrLf +
+                         "Totals: " + intTotal.ToString("n2"))
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        Dim strTeam As String = ""
+
+        strTeam = InputBox("Enter your favorite sports team", "Sports Team", "No favoritet team")
+
+        lstContainer.Items.Add(strTeam)
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        Dim Result As DialogResult
+        Result = MessageBox.Show("Are you sure you want to clear the list box?", "Clear Items", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1)
+
+        If Result = vbYes Then
+            lstContainer.Items.Clear()
+        End If
+    End Sub
+
+    Private Sub btnDoWhile_Click(sender As Object, e As EventArgs) Handles btnDoWhile.Click
+        Dim intX As Integer
+        Dim intTotal As Integer
+
+        intX = 1
+
+        Do While intX <= 10
+            intTotal += intX
+            lstContainer.Items.Add("intX is: " + intX.ToString("n0") + vbCrLf + "  intTotal is: " + intTotal.ToString("n0"))
+            intX += 1
+        Loop
+
+        MessageBox.Show("intX is: " + intX.ToString("n0") + vbCrLf + "intTotal is: " + intTotal.ToString("n0"))
+    End Sub
+
+    Private Sub btnDoUntil_Click(sender As Object, e As EventArgs) Handles btnDoUntil.Click
+        Dim intX As Integer
+        Dim intTotal As Integer
+
+        intX = 1
+
+        Do
+            intTotal += intX
+            lstContainer.Items.Add("intX is: " + intX.ToString("n0") + vbCrLf + "   intTotal is: " + intTotal.ToString("n0"))
+            intX += 1
+        Loop Until intX >= 10
+
+        MessageBox.Show("intX is: " + intX.ToString("n0") + vbCrLf + "intTotal is: " + intTotal.ToString("n0"))
+    End Sub
+
+    Private Sub btnUserInput_Click(sender As Object, e As EventArgs) Handles btnUserInput.Click
+        'Input Box Variables
+        Dim strInputMessage As String = "Enter in test score or cancel to end"
+        Dim strTitle As String = "Enter Test Score"
+        Dim strDefault As String = "0"
+        Dim strCancelClicked As String = ""
+
+        'Other Variables
+        Dim strTestScore As String = ""
+        Dim sngTestScore As Single = 0
+        Dim intCount As Integer = 0
+        Dim sngTotal As Single = 0
+        Dim sngAverage As Single = 0
+
+        'Get user input
+        strTestScore = InputBox(strInputMessage, strTitle, strDefault)
+
+
+        Do While strTestScore <> strCancelClicked
+            If IsNumeric(strTestScore) Then
+                'input is a number
+                sngTestScore = Convert.ToSingle(strTestScore)
+                'test to see if input is within range
+                If (sngTestScore >= 0 And sngTestScore <= 100) Then
+                    'test score is within range
+                    'add test score to the listbox
+                    lstContainer.Items.Add(sngTestScore.ToString("n2"))
+                    'add test score to the accumulator
+                    sngTotal += sngTestScore
+                    intCount += 1
+                Else
+                    'Test score is not within acceptable range
+                    MessageBox.Show("Test score must be within 0 and 100")
+                End If
+            Else
+                'input is not a number
+                MessageBox.Show("Please enter a valid number")
+            End If
+            strTestScore = InputBox(strInputMessage, strTitle, strDefault)
+        Loop
+        sngAverage = sngTotal / intCount
+
+        MessageBox.Show(intCount.ToString("n0") + " tests were entered" + vbCrLf +
+                        "Total Score: " + sngTotal.ToString("n2") + vbCrLf +
+                        "Average Score: " + sngAverage.ToString("n2"), "Test Results")
+    End Sub
+End Class
